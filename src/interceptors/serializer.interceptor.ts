6edf31a77432856d2f497405ff9785865a894659
aiden <campbell.e.aiden@gmail.com>
2021-09-15 20:24:09 +0200

    interceptor

---
+++ src/interceptors/serializer.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+export function Serilizer(dto) {
+    return @UseInterceptors(new CoustomeInterceptor(dto))
+}
+
+@Injectable()
+export class CoustomeInterceptor implements NestInterceptor {
+  constructor(private readonly dto) {}
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next
+      .handle()
+      .pipe(
+        map((data) =>
+          plainToClass(this.dto, data, { excludeExtraneousValues: true }),
+        ),
+      );
+  }
+}
